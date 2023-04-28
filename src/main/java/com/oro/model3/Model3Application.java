06e04f352d72f847b364e4920b6f0924df5a646c
szymonaszek01 <235879@edu.p.lodz.pl>
2023-04-28 15:40:51 +0200

    Initial commit

---
+++ src/main/java/com/oro/model3/Model3Application.java
@@ -0,0 +1,13 @@
+package com.oro.model3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Model3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Model3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oro/model3/Model3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oro.model3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Model3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
