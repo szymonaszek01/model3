699d1638a42b6168417d07e756cecb94f034ae69
szymonaszek01 <235879@edu.p.lodz.pl>
2023-04-29 21:48:46 +0200

    task-005 Adding class with project constants

---
+++ src/main/java/com/oro/model3/util/ConstantUtil.java
@@ -0,0 +1,10 @@
+package com.oro.model3.util;
+
+public class ConstantUtil {
+
+    public static final String STUDENT = "STUDENT";
+
+    public static final String HOST = "HOST";
+
+    public static final String SCIENTIST = "SCIENTIST";
+}
